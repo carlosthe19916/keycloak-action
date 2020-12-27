a1700ed62188bfcd089ddb104666fa161a5e5eaa
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-12-27 09:18:35 +0100

    fix locationo of kcadm.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-$JBOSS_HOME/kcadm.sh config credentials \
+$JBOSS_HOME/bin/kcadm.sh config credentials \
 --server $1 \
 --user $2 \
 --password $3 \
 --realm master \
 --client admin-cli
 
-eval $JBOSS_HOME/kcadm.sh $4
+eval $JBOSS_HOME/bin/kcadm.sh $4
