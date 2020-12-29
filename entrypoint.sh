d4c7b511fbf4e5074df3368ec0e6762f3ae4fbdc
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-12-29 09:44:29 +0100

    change entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -l
 
+realm="${4:-master}"
+client="${5:-admin-cli}"
+
 $JBOSS_HOME/bin/kcadm.sh config credentials \
 --server $1 \
 --user $2 \
 --password $3 \
---realm $4 \
---client $5
+--realm $realm \
+--client $client
 
 eval $JBOSS_HOME/bin/kcadm.sh $6
