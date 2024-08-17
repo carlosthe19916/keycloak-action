615edb5f720343bc72569e231e55884e430bc130
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2024-08-16 21:19:39 -0500

    est

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,17 @@ interval=3
 max_attempts=$(($6/interval));
 
 ## Wait until server is ready to continue
-# echo "Waiting for $1"
-# until (curl --output /dev/null --silent --head --fail $1); do
-#   if [ ${attempt_counter} -eq ${max_attempts} ];then
-#     echo "Max attempts reached"
-#     exit 1
-#   fi
+echo "Waiting for $1"
+until (curl --output /dev/null --silent --head --fail $1); do
+  if [ ${attempt_counter} -eq ${max_attempts} ];then
+    echo "Max attempts reached"
+    exit 1
+  fi
 
-#   printf '.'
-#   attempt_counter=$(($attempt_counter+1))
-#   sleep $interval
-# done
-sleep 20
+  printf '.'
+  attempt_counter=$(($attempt_counter+1))
+  sleep $interval
+done
 
 ## JBOSS_HOME is not set for the Quarkus based Keycloak server
 if [ -z ${JBOSS_HOME} ]; then
